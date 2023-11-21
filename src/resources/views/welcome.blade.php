7d4ae2e7a0df4c684115d47a80b6fde145f42d32
Ivan <ivandasilvamartin1@gmail.com>
2023-11-21 18:09:51 +0000

    sin dd

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -850,7 +850,6 @@
                 @endauth
             </div>
         @endif
-        {{ dd(env('APP_ENV'), env('APP_DEBUG')) }}
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
                 <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg"
